78e5febe490e8be8d2baff3d6c66dc79a745fb06
Gomathivinayagam <sankarmail@gmail.com>
2014-04-20 16:13:54 -0700

    Updating exception class

--- src/com/geoweb/exceptions/SystemUnavailableException.java
+++ src/com/geoweb/exceptions/SystemUnavailableException.java
@@ -1,5 +1,18 @@
 package com.geoweb.exceptions;
 
-public class SystemUnavailableException {
+public class SystemUnavailableException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SystemUnavailableException(String message) {
+		super(message);
+	}
+
+	public SystemUnavailableException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
 
 }
